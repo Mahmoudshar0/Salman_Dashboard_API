f897b768029948fccbf4ac7fa3924902d7d25a5e
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2026-03-17 13:08:36 +0200

    Initial Uploading

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import {connect} from "mongoose"
 
 dotenv.config();
 const PORT = process.env.PORT;
+const MODE = process.env.MODE;
+const MONGO_DB_URL = MODE === "DEV" ? process.env.LOCAL_MONGO_DB_URL : process.env.PIBLIC_MONGO_DB_URL;
 
+connect(MONGO_DB_URL)
+.then(() => {
+    console.log("Mongo DB connected successfully");
+})
+.catch((err) => {
+    console.log("Mongo DB connection error:", err);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}
+    in ${MODE} mode
+    with DB Uri : ${MONGO_DB_URL}`);
 });
